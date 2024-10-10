5dcc5a40b10003f452d9c89346b80cf2def24531
Saiful Islam <saiful.abir20@gmail.com>
2024-10-10 21:38:34 +0200

    path parameters

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/blog/all")
+def get_all_blogs():
+    return {"message": "All blogs provided"}
+
+
+@app.get("/blog/{id}")
+def get_blog(id: int):
+    return {"message": f"Blog with id {id}"}
