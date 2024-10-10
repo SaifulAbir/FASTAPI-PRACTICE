306ad52e3885f866ed413436da463d5297b1be55
Saiful Islam <saiful.abir20@gmail.com>
2024-10-10 21:59:28 +0200

    predefined path values with enum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from enum import Enum
 app = FastAPI()
 
 
@@ -16,6 +16,17 @@ async def say_hello(name: str):
 @app.get("/blog/all")
 def get_all_blogs():
     return {"message": "All blogs provided"}
+
+
+class BlogType(str, Enum):
+    short = "short"
+    story = "story"
+    howto = "howto"
+
+
+@app.get("/blog/type/{type}")
+def get_blog_type(type: BlogType):
+    return {"message": f"Blog type is {type.value}"}
 
 
 @app.get("/blog/{id}")
