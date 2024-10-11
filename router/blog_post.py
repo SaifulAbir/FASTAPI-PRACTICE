046a4c657c66ef0ff098014de7f041b24f6ee5f9
Saiful Islam <saiful.abir20@gmail.com>
2024-10-11 03:11:49 +0200

    metadata added for query parameter

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,18 +1,19 @@
 from typing import Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from pydantic import BaseModel
-
 
 router = APIRouter(
     prefix="/blog",
     tags=["blog"]
 )
 
+
 class BlogModel(BaseModel):
     title: str
     content: str
     nb_comments: int
     published: Optional[bool]
+
 
 @router.post('/new/{id}')
 def create_blog(blog: BlogModel, id: int, version: int = 1):
@@ -21,3 +22,14 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
         "data": blog,
         "version": version
     }
+
+
+@router.post('new/{id}/comment')
+def create_comment(blog: BlogModel, id: int,
+                   comment_id: int = Query(None,
+                                           title="Id of the comment",
+                                           description="Some description for comment_id",
+                                           alias='commentId',
+                                           deprecated=True)
+                   ):
+    return {'blog': blog, 'id': id, 'comment_id': comment_id}
