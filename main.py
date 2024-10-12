9b0ec51d77a9c54afb15c1dd8b9d2e15c8ee6add
Saiful Islam <saiful.abir20@gmail.com>
2024-10-12 22:45:14 +0200

    user create api done

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from db import models
 from db.database import engine
-from router import blog_get
+from router import blog_get, user
 from router import blog_post
 
 
 app = FastAPI()
+app.include_router(user.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
