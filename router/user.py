ce7f2fae67b4554158a5778eb85e29b3639a1420
Saiful Islam <saiful.abir20@gmail.com>
2024-10-13 00:58:51 +0200

    user read api done

--- router/user.py
+++ router/user.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db import db_user
@@ -14,7 +16,15 @@ router = APIRouter(
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
 
-# Read user
+# Read all users
+@router.get('/', response_model=List[UserDisplay])
+def get_all_users(db: Session = Depends(get_db)):
+    return db_user.get_all_users(db)
+
+# Read one user
+@router.get('/{id}', response_model=UserDisplay)
+def get_user(id: int, db: Session = Depends(get_db)):
+    return db_user.get_user(db, id)
 
 # Update user
 
